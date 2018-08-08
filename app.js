ae55da59ec19cfd20da93e5a256385d5f38eed7a
BoyLondon <fucshu@163.com>
2018-08-09 00:27:11 +0800

    add road & line & basic scene

--- app.js
+++ app.js
@@ -1,19 +1,60 @@
 var scene, camera, renderer;
+
+function create_line(line_length, line_width=50, line_height=2){
+    let geometry = new THREE.BoxGeometry(line_length, line_width, line_height)
+    let material = new THREE.MeshBasicMaterial( { color: 0xffd700} );
+    let cube = new THREE.Mesh(geometry, material);
+    return cube;
+}
+
+function change_position(cube, x, y ,z){
+    cube.position.x = x;
+    cube.position.y = y;
+    cube.position.z = z;
+}
+
+function create_line_with_position(line_length, line_width, line_height, x, y ,z){
+    var line = create_line(line_length, line_width, line_height);
+    change_position(line, x, y, z);
+    return line;
+}
+
 function init(){
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 10000 );
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor(0xffffff, 1.0);
 
     document.getElementById("canvas-frame").appendChild( renderer.domElement );
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+    var car_length = 447.3, car_width = 170.6;
+    var garage_width = car_width + 60, garage_length = car_length + 70;
+    var control_distance = car_length * 1.5, lane_width = car_length * 1.5;
+
+    var road_length = garage_length + 2 * control_distance + 2 * 200;
+    var road_width = lane_width + garage_length + 2 * 100;
+
+    console.log("rl:" + road_length, "rw:" + road_width);
+
+
+    var geometry = new THREE.BoxGeometry( road_length + 400, road_width + 200, 200 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x4f4f4f } );
     var cube = new THREE.Mesh( geometry, material );
+
     scene.add( cube );
 
-    camera.position.z = 5;
+    var line_width = 50, line_height = 2;
+    var topline_length = road_length;
+    var top_line = create_line(topline_length);
+    change_position(top_line , 0, 0, 100);
+    // top_line.position.z = 100;
+
+    scene.add(top_line);
+
+    camera.position.z = 1000;
 }
 
 function render(){
@@ -26,6 +67,10 @@ function animate(){
 }
 
 function threeStart() {
+
     init();
+
+    var controls = new THREE.OrbitControls(camera);
+    controls.addEventListener('change', render);
     animate();
 }
