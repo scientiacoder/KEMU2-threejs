4591637dfc9fc6610005a90cd7776af646f51264
BoyLondon <fucshu@163.com>
2018-08-09 17:40:58 +0800

    finish basic road render

--- app.js
+++ app.js
@@ -67,8 +67,8 @@ function init(){
 
     var line_width = 50, line_height = 2;
     var topline_length = road_length;
-    var top_line = create_line_with_position(topline_length, line_width, line_height, 0, 694.125, 100);
-    console.log("topline length:" + topline_length);
+    var top_line = create_line_with_position(topline_length, line_width, line_height, 0, 2 * 0.5 * line_width + lane_width, 100);
+
     console_log_position("top_line", top_line);
     scene.add(top_line);
 
@@ -78,6 +78,32 @@ function init(){
     scene.add(middle_line_left);
     console.log("middleline left position:" + middle_line_left.position)
     console_log_position("middle_line_left" ,middle_line_left);  
+
+    var left_vertical_middle_line = create_line(garage_length, line_width, line_height);
+    left_vertical_middle_line.rotation.z = 90 * Math.PI / 180;
+    change_position(left_vertical_middle_line, - (garage_width * 0.5 + 25), - (garage_length * 0.5), 100);
+
+
+    console_log_position("left_vertical_middle_line", left_vertical_middle_line);
+    scene.add(left_vertical_middle_line);
+
+    var right_vertical_middle_line = create_line(garage_length, line_width, line_height);
+    right_vertical_middle_line.rotation.z = 90 * Math.PI / 180;
+    change_position(right_vertical_middle_line, garage_width * 0.5 + 25, - (garage_length * 0.5), 100);
+    console_log_position("right_vertical_middle_line", right_vertical_middle_line);
+    scene.add(right_vertical_middle_line);
+
+    var bottom_line = create_line(garage_width, line_width, line_height);
+    change_position(bottom_line, 0, -(garage_length + 25), 100);
+    console_log_position("bottom_line",bottom_line);
+    scene.add(bottom_line);
+
+    var middle_line_right_length = middle_line_left_length;
+    var middle_line_right = create_line_with_position(middle_line_right_length, line_width, line_height,
+                                                        (garage_width + middle_line_right_length) * 0.5, 
+                                                        0, 100);
+    scene.add(middle_line_right);
+    console_log_position("middle_line_right", middle_line_right);                                                        
                                              
 
     camera.position.z = 1000;
